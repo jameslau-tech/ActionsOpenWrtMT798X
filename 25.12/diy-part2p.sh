0618ee7c91cb95a83cadcccf8f06d54f9c1ddbcc
jameslau-tech <Jameslau@126.com>
2026-06-17 11:35:12 +0800

    Update diy-part2p.sh

--- 25.12/diy-part2p.sh
+++ 25.12/diy-part2p.sh
@@ -90,7 +90,7 @@ fi
 #fi
 
 # 修改默认 IP (192.168.30.1)
-sed -i 's/192.168.1.1/192.168.20.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/JWRT/g' package/base-files/files/bin/config_generate
 #sed -i 's/hostname='.*'/hostname='JWRT'/g' package/base-files/files/bin/config_generate
 
