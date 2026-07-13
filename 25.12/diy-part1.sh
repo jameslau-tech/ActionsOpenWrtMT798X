7920de9d8b803dd634a87debb465b5effca41b9c
jameslau-tech <Jameslau@126.com>
2026-07-13 23:40:10 +0800

    Update diy-part1.sh

--- 25.12/diy-part1.sh
+++ 25.12/diy-part1.sh
@@ -46,7 +46,7 @@ git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/cust
 
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
-#git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
+git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/custom/OpenAppFilter
 #git clone --depth=1 https://github.com/janvanstiphout/luci-app-accesscontrol.git package/custom/accesscontrol
 
